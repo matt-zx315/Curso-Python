5596b50f984917e51ef4eaaca1c7be0044fabee5
Matheus <matheusagulerabraga.fab@gmail.com>
2020-01-30 10:58:42 -0300

    Arquivo de testes atualizado. Uso do módulo colored testado com sucesso.

--- Aula 1-Introdução/03_Teste.py
+++ Aula 1-Introdução/03_Teste.py
@@ -1,1 +1,14 @@
-print("OWARI DA!")
+from colored import *
+
+texto = "%s Isso é um texto! %s" % (fg(208), attr(0))
+
+print(texto)
+
+
+def soma(_a, _b):
+    return _a + _b
+
+
+resultado = f"%s {soma(6, 4)} %s" % (fg(196), attr(0))
+
+print(resultado)
