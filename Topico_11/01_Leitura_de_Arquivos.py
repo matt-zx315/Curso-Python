3e2831e39fc41d731e4f244770df744b966a4791
Matheus <matheusagulerabraga.fab@gmail.com>
2020-02-07 14:47:13 -0300

    Atualização do arquivo: explicação e exemplo da função close() e atributo closed.

--- Topico_11/01_Leitura_de_Arquivos.py
+++ Topico_11/01_Leitura_de_Arquivos.py
@@ -27,7 +27,28 @@ Para ler um arquivo, utilizamos a função integrada open()
  é posicionado no final do mesmo. Portanto, abrir o mesmo arquivo uma segunda vez não terá efeito algum,
  já que o cursor lerá apenas quilo que estiver na sua frente. Se o cursor está no fim do arquivo,
  então não haverá nada mais para ser lido. 
- 
+
+OBS.: Ao abrir um arquivo com a função open(), é criada uma conexão do arquivo
+com o disco do computador e o programa. Essa conexão é chamada de streaming.
+Ao encerrar os trabalhos com arquivos, usar a função close() para
+fechar essa conexão.
+
+Passos para trabalhar com arquivos:
+
+ 1 - Abrir o arquivo
+ 2 - Trabalhar o arquivo (leitura/escrita)
+ 3 - Fechar o arquivo
+
+Por que encerrar?
+
+ - Quando um arquivo é aberto por um programa, é criado um mecanismo chamado lock.
+ Esse mecanismo impede que outros programas acessem o arquivo aberto.
+ - Tentar acessar um arquivo fechado retorna um ValueError.
+ - Para verificar se um arquivo está aberto ou fechado, utiliza-se arquivo.closed.
+ - A verificação serve para evitar que esse erro ocorra. Dessa forma,
+ caso o arquivo esteja fechado, será necessário reabrí-lo, evitando assim o erro
+ e, consequentemente, a interrupção do programa.
+
 """
 
 arquivo = open("texto.txt")
@@ -41,3 +62,9 @@ print(len(leitura))  # Contando o número de caracteres.
 
 leitura = leitura.split('\n')
 print(len(leitura))  # Contando o número de linhas.
+
+print(arquivo.closed)  # Verifica se o arquivo está fechado.
+
+arquivo.close()
+
+print(arquivo.closed)
