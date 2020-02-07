9dfb58c543cea791368733309870584617a0bc1e
Matheus <matheusagulerabraga.fab@gmail.com>
2020-02-07 13:23:34 -0300

    Tópico 11 iniciado. Aula 1 adicionada: Leitura de arquivos e arquivo de teste.

---
+++ Topico_11/01_Leitura_de_Arquivos.py
@@ -0,0 +1,43 @@
+"""
+Leitura de arquivos
+
+Para ler um arquivo, utilizamos a função integrada open()
+
+ - open() -> Forma simples: passar apenas um argumento de entrada (arquivo ou caminho do arquivo a ser lido).
+ Retorna um objeto do tipo _io.IOWrapper.
+ Essa funçao abre um arquivo para leitura. Esse arquivo deve existir,
+ caso contrário, a função retorna o erro FileNotFoundError.
+
+ Ao imprimir um arquivo aberto com a função open:
+
+ <_io.TextIOWrapper name='texto.txt' mode='r' encoding='UTF-8'>
+         tipo           nome          modo      codificação
+
+ - tipo: tipo de objeto gerado pela função.
+ - nome: nome do arquivo aberto.
+ - modo: para quê o arquivo foi aberto. 'r' significa leitura (do inglês, "read").
+ - codificação: a codificação utilizada para escrever o arquivo.
+
+ - read() -> Lê o conteúdo do arquivo aberto.
+ Retorna um objeto do tipo string (str).
+ 
+ Caso o arquivo já tenha sido lido anteriormente, a função não exibirá nada.
+ Isso acontece por causa de um recurso do Python chamado cursor, o qual funciona da mesma forma
+ que o cursor quando se está escrevendo um arquivo. Toda vez que um arquivo é aberto, o cursor
+ é posicionado no final do mesmo. Portanto, abrir o mesmo arquivo uma segunda vez não terá efeito algum,
+ já que o cursor lerá apenas quilo que estiver na sua frente. Se o cursor está no fim do arquivo,
+ então não haverá nada mais para ser lido. 
+ 
+"""
+
+arquivo = open("texto.txt")
+leitura = arquivo.read()
+
+print(arquivo)
+print(type(arquivo))
+print(leitura)
+print(type(leitura))
+print(len(leitura))  # Contando o número de caracteres.
+
+leitura = leitura.split('\n')
+print(len(leitura))  # Contando o número de linhas.
